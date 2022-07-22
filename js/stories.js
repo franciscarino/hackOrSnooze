daa0cb7e87a9879b6d661fd61f3f1a8b6a15c2b6
Julie Han <juliemenghan@gmail.com>
2022-07-22 14:06:54 -0400

    added togglefavorites fx

--- js/stories.js
+++ js/stories.js
@@ -12,6 +12,8 @@ async function getAndShowStoriesOnStart() {
   putStoriesOnPage();
 }
 
+//TODO: checkfor favorites function & motidify generatemarkup star
+
 /**
  * A render method to render HTML for an individual Story instance
  * - story: an instance of Story
@@ -25,6 +27,9 @@ function generateStoryMarkup(story) {
   const hostName = story.getHostName();
   return $(`
       <li id="${story.storyId}">
+        <span class="star">
+          <i class="bi bi-star"></i>
+        </span>
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
@@ -60,10 +65,11 @@ async function submitNewStory() {
   const url = $("#create-url").val();
 
   const newStorySubmission = await storyList.addStory(currentUser,
-    {title,
-    author,
-    url
-  });
+    {
+      title,
+      author,
+      url
+    });
 
   const $newStory = generateStoryMarkup(newStorySubmission);
   $allStoriesList.prepend($newStory);
@@ -75,3 +81,37 @@ async function submitNewStory() {
 /** Listen for click on submit form */
 
 $submitForm.on("click", "#submit-story", submitNewStory);
+
+/** TO DO:
+ * "ON CLICK" -> star icon
+ * 1. toggles icon from blank star to filled star in the DOM (seperate fx)
+ * 2. check if parent li for storyID (2-5 is another fx)
+ * 3. check if storyID is in currentUser.favorites
+ * 4. if yes -> call unfavorite fx
+ * 5. if no -> call addfavorite fx
+ */
+function toggleIcon(star) {
+  //if star i class = bi bi-star -> toggle to bi bi-star-fill
+  const $icon = star.children("i");
+  $icon.attr("class") === "bi bi-star" ?
+    $icon.attr("class", "bi bi-star-fill") :
+    $icon.attr("class", "bi bi-star");
+    //TODO: check jQuery toggle class
+}
+
+//toggle storyFavorites
+function toggleStoryFavorite(favoriteStory) {
+const $story = favoriteStory.parent("li");
+const storyId =$story.attr("id");
+
+currentUser.favorites.includes(story) ? //check if its already favorited
+  currentUser.unfavorite(story) : //TODO: get story by story ID
+  currentUser.addFavorite(story);
+}
+
+//global on click fxfunction toggleStoryFavorite
+$allStoriesList.on("click", function (evt) {
+  const star = $(evt.target).closest(".star");
+  toggleIcon(star);
+
+});
